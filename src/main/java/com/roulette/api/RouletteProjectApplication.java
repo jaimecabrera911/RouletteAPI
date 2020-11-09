ce1fa26c930a6857cf79fde7364e3eff539186f0
jaime.cabrera <cabrerajaimeandres@gmail.com>
2020-11-09 08:08:45 -0500

    Initial configuration project MySQL and H2 connection

---
+++ src/main/java/com/roulette/api/RouletteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.roulette.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteProjectApplication.class, args);
+	}
+
+}
